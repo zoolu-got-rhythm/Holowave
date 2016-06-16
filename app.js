a34c64fa6f5eba45487b98d37ffcfce5673509c2
Christopher P.K Morris <christopher.pkm@gmail.com>
2016-06-16 21:56:18 +0100

    api v0.1 is functional but not tested

--- app.js
+++ app.js
@@ -2,103 +2,134 @@
  * Created by Slime on 14/06/2016.
  */
 
-var c = document.createElement("canvas");
-var ctx = c.getContext("2d");
-c.width = 80;
-c.height = 40;
-document.body.appendChild(c);
 
-function Shape(x, y, i){
-    this.x = x;
-    this.y = y;
-    this.height = 20;
-    this.width = 1;
-    this.i = i;
-    this.toggle = true;
-    this.myLoopReq;
 
-    this.init();
-}
+// module
+var holoWave = (function(){
 
-Shape.prototype.move = function(n){
-    this.x = n;
-}
+    // encapsulate code from global namespace
 
-Shape.prototype.draw = function(){
-    this.fluctuate();
+    var c = document.createElement("canvas");
+    var ctx = c.getContext("2d");
+    c.width = 80;
+    c.height = 40;
+    document.body.appendChild(c);
 
-    ctx.strokeStyle = "#c61aff";
+    function Shape(x, y, i){
+        this.x = x;
+        this.y = y;
+        this.height = 20;
+        this.width = 1;
+        this.i = i;
+        this.toggle = true;
+        this.speed = 10;
+        this.myLoopReq;
 
-    // canvas api
-    ctx.clearRect(this.x - 1, (this.y - this.i) - 1, this.x + 1, (this.y + this.i) + 1);
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y - this.i);
-    ctx.lineTo(this.x, this.y + this.i);
-    ctx.stroke();
-}
-
-Shape.prototype.fluctuate = function(){
-
-    //if(this.i > 10)this.i = 10;
-
-    if(this.toggle){
-        this.i++;
-        if(this.i >= 10) this.toggle = false;
-    } else{
-        this.i--;
-        if(this.i === 0) this.toggle = true;
+        this.init();
     }
-    return this.i;
-}
 
-Shape.prototype.wave = function(){
-    var self = this;
-    //window.setInterval(loop, 800);
-    this.myLoopReq = window.requestAnimationFrame(loop);
-
-    function loop(){
-        // console.log("looping..");
-        self.draw();
-        window.requestAnimationFrame(loop);
+    Shape.prototype.move = function(n){
+        this.x = n;
     }
-}
 
-Shape.prototype.init = function(){
-    console.log("ran");
-    this.wave();
-}
+    Shape.prototype.draw = function(){
+        this.fluctuate();
 
-Shape.prototype.kill = function(){
-    window.cancelAnimationFrame(this.myLoopReq);
-}
+        ctx.strokeStyle = "#c61aff";
+        this.wipe();
 
-var lines = [];
-// var i = 0;
-
-// makes 6 lines
-for(var i = 0; i < 24; i += 4){
-    lines.push(new Shape(20 + i + i, 20, i));
-}
-
-
-
-
-function destroy(){
-    for(var j = 0; j < lines.length; j++){
-        lines[j].kill();
+        // canvas api
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y - this.i);
+        ctx.lineTo(this.x, this.y + this.i);
+        ctx.stroke();
     }
-}
+
+    Shape.prototype.fluctuate = function(){
+
+        //if(this.i > 10)this.i = 10;
+
+        if(this.toggle){
+            this.i++;
+            if(this.i >= 10) this.toggle = false;
+        } else{
+            this.i--;
+            if(this.i === 0) this.toggle = true;
+        }
+        return this.i;
+    }
+
+    Shape.prototype.wave = function(){
+        var self = this;
+        //window.setInterval(loop, 800);
+        this.myLoopReq = window.setInterval(loop, this.speed);
+
+        function loop(){
+            self.draw();
+        }
+    }
+
+    Shape.prototype.init = function(){
+        console.log("ran");
+        this.wave();
+    }
+
+    Shape.prototype.freeze = function(){
+        window.clearInterval(this.myLoopReq);
+        console.log(this.myLoopReq);
+    }
+
+    Shape.prototype.eraze = function(){
+        window.clearInterval(this.myLoopReq);
+        this.wipe();
+    }
+
+    Shape.prototype.wipe = function(){
+        ctx.clearRect(this.x - 1, (this.y - this.i) - 1, this.x + 1, (this.y + this.i) + 1);
+    }
 
 
-var loader = (function(){
+    var lines = [];
 
+    // api
     return {
-        start: function () {},
-        stop: function () {}
+        // you can change the internals of the code after the api is released
+        play: function (parent, colourHex, speed) {
+            // implementation
+            for(var i = 0; i < 24; i += 4){
+                lines.push(new Shape(20 + i + i, 20, i));
+            }
+        },
+
+        pause: function () {
+            // implementation
+            console.log("destroy ran");
+            if(lines.length > 0){
+                console.log("destroy ran");
+                for(var j = 0; j < lines.length; j++){
+                    console.log("killing");
+                    lines[j].freeze();
+                }
+            }
+        },
+
+        stop: function(){
+            // implementation
+            if(lines.length > 0){
+                while(lines.length > 0){
+                    lines[lines.length - 1].eraze();
+                    lines.pop();
+                }
+            }
+
+        }
     }
 
 
-}())
+}());
+
+
+holoWave.play();
 
 
 
